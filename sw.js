4a3b1646c7ca1ae09cc959d8b9e6aab315aa6d57
crisen84 <crisen84@msn.com>
2026-09-15 03:15:53 -0600

    Bump service worker version to v11
    Updated service worker version to trigger cache refresh.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Cambia VERSION cuando publiques una actualización: eso obliga a
    descargar de nuevo los archivos y borra la caché anterior. */
 
-const VERSION = "buscar-nfc-v10";
+const VERSION = "buscar-nfc-v11";
 
 const ARCHIVOS = [
   "./",
