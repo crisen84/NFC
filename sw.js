d4c729ab90aa931b25094b91a68638338dcb492a
crisen84 <crisen84@msn.com>
2026-09-15 02:04:44 -0600

    Bump service worker version to buscar-nfc-v8
    Updated service worker version to v8 to trigger cache refresh.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Cambia VERSION cuando publiques una actualización: eso obliga a
    descargar de nuevo los archivos y borra la caché anterior. */
 
-const VERSION = "buscar-nfc-v7";
+const VERSION = "buscar-nfc-v8";
 
 const ARCHIVOS = [
   "./",
