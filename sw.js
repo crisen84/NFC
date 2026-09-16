0dbc1a4f4ad27a88d84f987bce54e0f62e265e6b
crisen84 <crisen84@msn.com>
2026-09-16 01:39:10 -0600

    Downgrade service worker version to v17

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Cambia VERSION cuando publiques una actualización: eso obliga a
    descargar de nuevo los archivos y borra la caché anterior. */
 
-const VERSION = "buscar-nfc-v18";
+const VERSION = "buscar-nfc-v17";
 
 const ARCHIVOS = [
   "./",
